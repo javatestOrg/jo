920fd22bc4abd10a5a371813d405195473e033eb
Srividhya Varadhan <100423362+Srividhya0504@users.noreply.github.com>
2023-04-03 18:45:21 +0530

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 
 import java.io.Serializable;
-
+/*
 @RestController
 @EnableAutoConfiguration
 public class CowController {
     @RequestMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
-        return Cowsay.run(input);
+        return Cowsay.run(input);*/
     }
 }
